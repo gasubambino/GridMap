7c25309c72321f70387a57b367a457f6178a020b
gasubambino <fernandonunesferreira12@gmail.com>
2023-06-26 20:16:07 -0300

    first commit

---
+++ Assets/GridManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+
+    void GenerateGrid()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+
+            }
+        }
+    }
+}
