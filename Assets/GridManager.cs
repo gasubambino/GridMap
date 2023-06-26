a86d4b1cdad99860e6c334a405d539298cedf9cf
gasubambino <fernandonunesferreira12@gmail.com>
2023-06-26 20:28:39 -0300

    .

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int width, height;
+    [SerializeField] private Tile tilePrefab;
 
+    private void Start()
+    {
+        GenerateGrid();
+    }
     void GenerateGrid()
     {
         for(int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(i*2,j*2),Quaternion.identity);
             }
         }
     }
